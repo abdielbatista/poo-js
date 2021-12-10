211d2a08f2f3f2b41caaf1a762c28f31d5ae576f
Abdiel Batista dos Santos <abdielb@yahoo.com>
2021-12-10 11:47:13 -0300

    create poo js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+class Produto{
+    constructor(){
+        this.id
+        this.nome
+        this.valor
+    }
+
+    create(){
+        alert("Você vai adicionar");
+    }
+
+    delete(){
+
+    }
+
+    update(){
+
+    }
+
+}
+
+var produto = new Produto();
